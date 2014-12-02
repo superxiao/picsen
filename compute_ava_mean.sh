4328c184e0594c939a68659eab746dd561753d9c
Xiaojian Wang <glancky@gmail.com>
2014-12-02 13:41:40 -0800

    Modified some preprocessing scripts

--- compute_ava_mean.sh
+++ compute_ava_mean.sh
@@ -5,6 +5,6 @@ $HOME/caffe/build/tools/compute_image_mean.bin \
 ava_train_lmdb ava_train_mean.binaryproto lmdb
 
 $HOME/caffe/build/tools/compute_image_mean.bin \
-ava_val_lmdb ava_test_mean.binaryproto lmdb
+ava_val_lmdb ava_val_mean.binaryproto lmdb
 
 echo "Done."
--- resize_extreme_high_pics.py
+++ resize_extreme_high_pics.py
@@ -1,28 +1,32 @@
 import os
 import Image
 import common
+import shutil
 
 HIGHT_RAITING_PERC = 0.1
 LOW_RAITING_PERC = 0.1
 
 def resize_and_save(imgpath):
 	img = Image.open(imgpath)
-	resized = img.resize((256, 256), Image.ANTIALIAS)
+    #resized = img.resize((256, 256), Image.ANTIALIAS)
 	print "Resized image " + cells[1]
-	resized.save("../ava_resized_extreme/"+cells[0]+"-"+cells[1]+".jpg")
+	img.save("../high_pics/"+cells[0]+"-"+cells[1]+".jpg")
 
 high_list = common.get_high_list(HIGHT_RAITING_PERC)
 low_list = common.get_low_list(LOW_RAITING_PERC)
 i = 0
-for high in high_list:
+for high in reversed(high_list):
 	i += 1
 	print str(i) + ":"
 	line = high[0]
 	cells = line.split(" ")
 	imgpath = "../ava/"+cells[0]+"-"+cells[1]+".jpg"
 	if(os.path.exists(imgpath)):
-		resize_and_save(imgpath)
+		shutil.copyfile(imgpath, "../high_pics/"+cells[0]+"-"+cells[1]+".jpg")
+		#resize_and_save(imgpath)
 	else:
 		print "Image " + cells[1] + " not found yaa! Downloading..."
 		common.download_image(line)
 		resize_and_save(imgpath)
+	if (i > 2000):
+		break;
